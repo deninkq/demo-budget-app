218fcf73e05543dffcbdc5ea363b4652f57d0b90
deninkq <65303267+deninkq@users.noreply.github.com>
2021-06-05 10:40:30 +0300

    added routing

--- budget-app/src/app/app-routing.module.ts
+++ budget-app/src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { StatisticComponent } from './statistic/statistic.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'statistic', pathMatch: 'full', component: StatisticComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { MaterialModule } from './material/material.module';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { StatisticComponent } from './statistic/statistic.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, StatisticComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
